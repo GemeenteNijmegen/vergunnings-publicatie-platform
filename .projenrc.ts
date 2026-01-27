00bfcfdad80fb39db309fe33766a7ee47de98bcc
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2026-01-27 11:37:01 +0100

    chore: Upgrade pipeline to use pinned node version
    Fixes pipelines.

--- .projenrc.ts
+++ .projenrc.ts
@@ -2,14 +2,20 @@ import { GemeenteNijmegenCdkApp } from '@gemeentenijmegen/projen-project-type';
 const project = new GemeenteNijmegenCdkApp({
   cdkVersion: '2.2.0',
   defaultReleaseBranch: 'acceptance',
-  devDeps: ['@gemeentenijmegen/projen-project-type'],
+  devDeps: [],
   name: 'vergunnings-publicatie-platform',
   projenrcTs: true,
   deps: [
     '@pepperize/cdk-ssm-parameters-cross-region',
     '@aws-solutions-constructs/aws-cloudfront-s3',
     '@gemeentenijmegen/aws-constructs',
+    '@gemeentenijmegen/projen-project-type',
   ],
+  tsconfig: {
+    compilerOptions: {
+      isolatedModules: true,
+    },
+  },
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
